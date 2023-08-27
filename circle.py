83a7f40d99145566d8c4b592a0db18d4fc80a157
KalminX <noriabure.com>
2023-08-26 22:29:27 -0800

    Created function that calculates area of a square

---
+++ circle.py
@@ -0,0 +1,9 @@
+#My python code
+
+#A function that calculates the area of a square
+def squareArea(radius):
+	area = 3.142 * (radius ** 2)
+	return (area)
+
+radius = int(input("Enter the radius of the circle in centimeters: "))
+print(f"The area of the circle of radius {radius} is {squareArea(radius)} centimeters square.")
